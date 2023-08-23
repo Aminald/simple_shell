f7dfee9df0c9f52b6a1ed3ad0156691ee8a9ca4d
root <laghidia@gmail.com>
2023-08-22 17:27:21 -0700

    task1

--- my_shell.c
+++ my_shell.c
@@ -1,54 +1,71 @@
+#include "shell.h"
+
+int main(void) {
+    run_shell();
+    return 0;
+}
+
+void run_shell(void) {
+    // Your main code implementation here
+    // ...
+}
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define MAX_INPUT_LENGTH 1024
+#define BUFFER_SIZE 1024
 
 int main(void)
 {
-    char input[MAX_INPUT_LENGTH];
-    char *args[2]; // We only expect one argument (the command)
-
+    char buffer[BUFFER_SIZE];
+    char *prompt = "#cisfun$ ";
+    ssize_t read_bytes;
+    
     while (1)
     {
-        printf("#cisfun$ ");
-        fflush(stdout);
-
-        if (fgets(input, sizeof(input), stdin) == NULL) // Read user input
+        printf("%s", prompt);
+        
+        read_bytes = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+        if (read_bytes == 0) // Ctrl+D (EOF) handling
         {
             printf("\n");
-            break; // Handle Ctrl+D (EOF)
-        }
-
-        input[strcspn(input, "\n")] = '\0'; // Remove newline character
-
-        if (strcmp(input, "exit") == 0) // Exit the shell
             break;
-
-        args[0] = input;
-        args[1] = NULL; // Terminate the args array
-
+        }
+        else if (read_bytes == -1)
+        {
+            perror("read");
+            exit(EXIT_FAILURE);
+        }
+        
+        buffer[read_bytes - 1] = '\0'; // Remove the newline character
+        
+        if (strcmp(buffer, "exit") == 0) // Exit the shell
+            break;
+        
         pid_t pid = fork();
         if (pid == -1)
         {
             perror("fork");
             exit(EXIT_FAILURE);
         }
-        else if (pid == 0) // Child process
+        else if (pid == 0)
         {
-            if (execvp(args[0], args) == -1)
+            // Child process
+            if (execve(buffer, NULL, NULL) == -1)
             {
-                perror("execvp");
+                perror("execve");
                 exit(EXIT_FAILURE);
             }
         }
-        else // Parent process
+        else
         {
-            int status;
-            waitpid(pid, &status, 0); // Wait for child to finish
+            // Parent process
+            wait(NULL);
         }
     }
-
+    
     return 0;
 }
+
---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+// Function to read user input and execute commands
+void run_shell(void);
+
+#endif /* SHELL_H */
+
